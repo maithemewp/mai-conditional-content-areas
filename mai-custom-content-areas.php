df0a3fb76f3a33f5090e5e03498ae19f1f112b9e
Mike Hemberger <mike@thestizmedia.com>
2021-10-29 17:21:17 -0400

    0.1.1

--- mai-custom-content-areas.php
+++ mai-custom-content-areas.php
@@ -4,7 +4,7 @@
  * Plugin Name:     Mai Custom Content Areas
  * Plugin URI:      https://bizbudding.com/mai-theme/plugins/mai-custom-content-areas/
  * Description:     Display content, calls to action, ads, etc. on posts, pages, and custom post types conditionally by category, tag, taxonomy, entry title, and more.
- * Version:         0.1.0
+ * Version:         0.1.1
  *
  * Author:          BizBudding
  * Author URI:      https://bizbudding.com
